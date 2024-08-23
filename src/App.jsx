ef14e535625d52c9a0e3ae75de5c961c37e5fb99
Julius Olajumoke <juliusolajumokecoder@gmail.com>
2024-08-23 19:32:29 +0100

    preping for deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+/**
+ * Project: Realtime Weather Application 
+ * Author: Julius Olajumoke
+ * Email: devcalledjulius@gmail.com
+ * Version: v1.0.0
+ */
+
 import WeatherApp from "./components/WeatherApp"
 
 const App = () => {
